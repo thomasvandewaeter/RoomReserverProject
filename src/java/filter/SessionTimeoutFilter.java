632dad8eba9329c7f23de762cd6c1f3e731fa5bd
MatteusDeloge <matteus.deloge2@gmail.com>
2016-06-15 19:02:04 +0200

    Tried to get filter working Added validation back to registration page

--- src/java/filter/SessionTimeoutFilter.java
+++ src/java/filter/SessionTimeoutFilter.java
@@ -14,33 +14,26 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Matteus
  */
-@WebFilter(filterName = "SessionTimeoutFilter")
+@WebFilter(filterName = "SessionTimeoutFilter") //urlPatterns = "*.xhtml" ??????
 public class SessionTimeoutFilter implements Filter {
     
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-
+/*
         HttpServletRequest req = (HttpServletRequest) request;
-
-        HttpSession session = req.getSession(false);
-
-        // if session doesn't exist, forward user to welcome page
-        if (session == null) {
-            try {
-                req.getRequestDispatcher("/welcome.xhtml").forward(request, response);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        if(req.getRequestedSessionId() != null && !req.isRequestedSessionIdValid()){
+            res.sendRedirect(req.getContextPath() + "/welcome.xhtml");
         }
 
-        chain.doFilter(request, response);
+        chain.doFilter(request, response);*/
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {}
